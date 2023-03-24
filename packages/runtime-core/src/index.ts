ff385a1f0b35839a9a56ed36e2cc783e34769123
王鑫 <wb_wangxin06@kuaishou.com>
2023-03-24 11:35:28 +0800

    KeepAlive组件相关api

--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -17,3 +17,12 @@ export * from './component'
 
 // 依赖注入相关api;
 export * from './apiInject'
+
+//传送门组件相关api;
+export { TeleportImpl as Teleport } from './components/Teleport'
+
+// KeepAlive组件相关api;
+export { KeepAliveImpl as KeepAlive } from './components/KeepAlive'
+
+//异步组件相关api
+export * from './defineAsyncComponent'
