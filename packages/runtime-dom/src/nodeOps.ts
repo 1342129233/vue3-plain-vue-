9ad0cd9510879f963dc034b702bfb78afed95282
王鑫 <wb_wangxin06@kuaishou.com>
2023-03-24 15:42:49 +0800

    节点方法

--- packages/runtime-dom/src/nodeOps.ts
+++ packages/runtime-dom/src/nodeOps.ts
@@ -40,3 +40,20 @@ export const nodeOps = {
     return document.createTextNode(text)
   }
 }
+
+/* //要写的方法;
+const fang = {
+  insert: hostInsert,//插入节点;
+  remove: hostRemove,//删除节点;
+  patchProp: hostPatchProp,//属性操作,要单独去写;//patch实际上就是补丁或比对的意思;//因为这个方法即要能创建,又要能修改,还要能移除;
+  createElement: hostCreateElement,//创建元素;
+  createText: hostCreateText,//创建文本;
+  createComment: hostCreateComment,//创建注释;
+  setText: hostSetText,//设置文本节点文本;
+  setElementText: hostSetElementText,//设置元素节点文本;
+  parentNode: hostParentNode,//获取父节点;
+  nextSibling: hostNextSibling,//获取兄弟节点;
+  setScopeId: hostSetScopeId = NOOP,//`NOOP`就是空函数的意思;
+  cloneNode: hostCloneNode,//把节点拷贝;
+  insertStaticContent: hostInsertStaticContent,//插入静态内容;
+} */
